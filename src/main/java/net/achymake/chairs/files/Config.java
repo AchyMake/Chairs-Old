5278b962fbbe2d2ab29c3af60c42d77394a17283
Anders <achymake@gmail.com>
2023-05-09 16:59:23 +0200

    cleaned up

--- src/main/java/net/achymake/chairs/files/Config.java
+++ src/main/java/net/achymake/chairs/files/Config.java
@@ -1,18 +1,25 @@
 package net.achymake.chairs.files;
 
 import net.achymake.chairs.Chairs;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Config {
     private static final File file = new File(Chairs.getInstance().getDataFolder(), "config.yml");
-    private static FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+    private static final FileConfiguration config = Chairs.getInstance().getConfig();
     public static FileConfiguration get() {
         return config;
     }
     public static void reload() {
-        config = YamlConfiguration.loadConfiguration(file);
+        try {
+            config.load(file);
+            config.options().copyDefaults(true);
+            config.save(file);
+        } catch (IOException | InvalidConfigurationException e) {
+            Message.sendLog(e.getMessage());
+        }
     }
 }
